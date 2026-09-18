6b519144c7cbec7cc5d6e0e0f96cb4162bf47a9c
Dark boy <tstarkx103@gmail.com>
2026-09-18 10:39:18 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 223
-        versionName = "6.1.3"
+        versionName = "6.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // Strip out language resources from libraries that the app doesn't support
